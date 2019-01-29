7263ebf2367bc66dd6f9ea3cf37c5a402521282e
Nuggy Santoso <nuggysantoso@Nuggys-MacBook-Pro.local>
2019-01-29 09:30:14 +0700

    Remove myStaffRequestPage Class

--- page/myStaffRequestPage.js
+++
@@ -1,7 +0,0 @@
-import { BasePage } from "./BasePage";
-
-export class MyStaffRequestPage extends BasePage{
-    myStaffRequestTxtView(){
-        return this.selectElementWithText('p', 'My Staff Requests')
-    }
-}
